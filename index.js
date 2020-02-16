4e836bf851ed7739902fcd696177623082c2a9f9
Yuriy Ko <kovalenko_juri@mail.ru>
2020-02-16 16:56:36 +0300

    Use sync function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const os = require('os');
-const { execFile } = require('child_process');
+const { execFileSync } = require('child_process');
 var process = require('process');
 var path = require('path');
 var fs = require('fs');
@@ -9,42 +9,27 @@ var cpus = os.cpus().length;
 console.log(`CPUs: ${cpus}`);
 console.log(`Starting directory:  ${process.cwd()}`);
 
-const git = execFile('git', ['submodule', 'update', '--init', '--recursive'], (error, stdout, stderr) => {
-    if (error) {
-      throw error;
-    }
-    console.log(stdout);
-  });
-
-var dirName = 'build';
-var buildPath = path.join(process.cwd(), dirName);
-
-if (!fs.existsSync(buildPath))
-{
-    fs.mkdirSync(buildPath);
-}
-
 try 
 {
+    const git = execFileSync('git', ['submodule', 'update', '--init', '--recursive']);
+
+    var dirName = 'build';
+    var buildPath = path.join(process.cwd(), dirName);
+
+    if (!fs.existsSync(buildPath))
+    {
+        fs.mkdirSync(buildPath);
+    }
+
     process.chdir(buildPath);
     console.log(`New directory: ${process.cwd()}`);
+
+    const cmakeConfigure = execFileSync('cmake', ['..']);    
+    const cmakeBuild = execFileSync('cmake', ['--build', '.', '-j', `${cpus}`]);
 } 
 catch (err) 
 {
     console.error(`chdir: ${err}`);
 } 
 
-const cmakeConfigure = execFile('cmake', ['..'], (error, stdout, stderr) => {
-    if (error) {
-      throw error;
-    }
-    console.log(stdout);
-});
 
-
-const cmakeBuild = execFile('cmake', ['--build', '.', '-j', `${cpus}`], (error, stdout, stderr) => {
-    if (error) {
-      throw error;
-    }
-    console.log(stdout);
-});
