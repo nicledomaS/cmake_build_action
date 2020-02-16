dc342c4fa1db962aac8d5053c53ef54588e8d417
Yuriy Ko <kovalenko_juri@mail.ru>
2020-02-16 14:07:54 +0300

    git checkout

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+const os = require('os');
+const { execFile } = require('child_process');
+
+var cpus = os.cpus();
+
+console.log(cpus.length);
+
+const child = execFile('git', ['submodule', '--init', '--recursive'], (error, stdout, stderr) => {
+    if (error) {
+      throw error;
+    }
+    console.log(stdout);
+  });
