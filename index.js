733fcba66796e67d9b9b87d98a50d9dad4cd4c61
Yuriy Ko <kovalenko_juri@mail.ru>
2020-02-16 14:34:55 +0300

    Test cmake

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const os = require('os');
 const { execFile } = require('child_process');
+const path = require('path');
+
 
 var cpus = os.cpus();
 
@@ -11,3 +13,17 @@ const child = execFile('git', ['submodule', 'update', '--init', '--recursive'], 
     }
     console.log(stdout);
   });
+
+const child = execFile('cmake', ['-B', 'build'], (error, stdout, stderr) => {
+    if (error) {
+      throw error;
+    }
+    console.log(stdout);
+});
+
+const child = execFile('cmake', ['--build=build'], (error, stdout, stderr) => {
+    if (error) {
+      throw error;
+    }
+    console.log(stdout);
+});
