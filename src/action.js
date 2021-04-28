78fc94a7c3676946e40365011f8094ee073bdb5b
nicledomaS <kovalenko_juri@mail.ru>
2021-04-28 17:44:22 +0300

    Revert "Fix unit tests"

--- src/action.js
+++ src/action.js
@@ -1,8 +1,3 @@
-const { Telegraf } = require('telegraf')
-
-const ttoken = 't_token';
-const tChatId = 't_chat_id';
-
 module.exports = class Action
 {
     constructor(core, execFileSync)
@@ -10,16 +5,6 @@ module.exports = class Action
         this._executors = new Array();
         this._core = core
         this._execFileSync = execFileSync
-        this._bot = null
-        this._chatId = null
-
-        const token = core.getInput(ttoken, { required: false })
-        if(token != "")
-        {
-            this._bot = new Telegraf(token)
-
-            this._chatId = core.getInput(tChatId, { required: false })
-        }
     }
 
     run()
@@ -28,23 +13,13 @@ module.exports = class Action
         {
             for(const executor of this._executors)
             {
-                let res = executor.execute(this._execFileSync)
-            }
-
-            if(this._bot)
-            {
-                this._bot.telegram.sendMessage(this._chatId, "build successful")
+                executor.execute(this._execFileSync);
             }
         } 
         catch (error)
         {
             this._core.setFailed(error.message);
             this._core.info(error.stdout);
-
-            if(this._bot)
-            {
-                this._bot.telegram.sendMessage(this._chatId, error.message)
-            }
         }
     }
 
